a0ef8351839de01ca6ca77c2523226cd385b1b08
osamikoyo <v.pylayev@mail.ru>
2025-09-22 13:08:10 +0500

    update handler

--- handler/handler.go
+++ handler/handler.go
@@ -2,21 +2,38 @@ package handler
 
 import (
 	"filer/config"
+	"filer/core"
+	"filer/templates"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type Handler struct {
-	cfg *config.Config
+	cfg  *config.Config
+	core *core.FileServerCore
 }
 
-func NewHandler(cfg *config.Config) *Handler {
+func NewHandler(core *core.FileServerCore, cfg *config.Config) *Handler {
 	return &Handler{
-		cfg: cfg,
+		cfg:  cfg,
+		core: core,
 	}
 }
 
-func (h *Handler) RegisterRoutes(mux *http.ServeMux) {
-	mux.HandleFunc("/upload", h.Upload)
-	mux.HandleFunc("/download/", h.Download)
-	mux.HandleFunc("/ping", h.PingHandler)
+func (h *Handler) RegisterRoutes(r *chi.Mux) {
+	r.Get("/ping", h.PingHandler)
+	r.Get("/list", h.ListFiles)
+	r.Post("/upload", h.Upload)
+	r.Get("/download/{filename}", h.Download)
+	r.Get("/delete/{filename}", h.Delete)
+	r.Get("/", h.Index)
+}
+
+func (h *Handler) Index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	templates.Main().Render(r.Context(), w)
 }
